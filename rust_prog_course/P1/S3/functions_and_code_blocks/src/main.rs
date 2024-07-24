fd8294d43f239c3ca20925cc5e702b705b88a3a3
Pain <ravi.ameer@gmail.com>
2024-07-24 16:56:51 +0530

    Basic math with multi return functions.

--- rust_prog_course/P1/S3/functions_and_code_blocks/src/main.rs
+++ rust_prog_course/P1/S3/functions_and_code_blocks/src/main.rs
@@ -10,6 +10,9 @@ fn main() {
 
     let multiplication_result = basic_multiplication(123, 345);
     println!("Result is {multiplication_result}");
+
+    let (addition, subtraction, multiplication) = basic_math(12, 34);
+    println!("Result of addition is {addition}\nResult of subtraction is {subtraction}\nResult of multiplication is {multiplication}");
 }
 
 fn my_fn(s: &str) {
@@ -19,4 +22,10 @@ fn my_fn(s: &str) {
 fn basic_multiplication(num1: i32, num2: i32) -> i32 {
     println!("Computing multiplication!");
     num1 * num2
+}
+
+// Multi return
+fn basic_math(num1: i32, num2: i32) -> (i32, i32, i32) {
+    println!("Basic math results!");
+    (num1 + num2, num1 - num2, num1 * num2)
 }
