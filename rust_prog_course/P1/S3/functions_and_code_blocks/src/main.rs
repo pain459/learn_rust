80ae83fbfd28478165a158c543e655414a27292c
Pain <ravi.ameer@gmail.com>
2024-07-24 17:06:54 +0530

    Using macro in a function.

--- rust_prog_course/P1/S3/functions_and_code_blocks/src/main.rs
+++ rust_prog_course/P1/S3/functions_and_code_blocks/src/main.rs
@@ -3,6 +3,8 @@
 // 			- Code Blocks
 // -------------------------------------------------
 
+use std::fmt::Debug;
+
 fn main() {
     my_fn("This is from my_fn function!");
     let str = "This is a custom string passed to the function!";
@@ -13,6 +15,14 @@ fn main() {
 
     let (addition, subtraction, multiplication) = basic_math(12, 34);
     println!("Result of addition is {addition}\nResult of subtraction is {subtraction}\nResult of multiplication is {multiplication}");
+
+    let full_name: String = {
+        let first_name = "Naruto";
+        let last_name = "Uzumaki";
+        format!("{first_name} {last_name}")
+    };
+
+    println!("The name is {:?}!", full_name);
 }
 
 fn my_fn(s: &str) {
