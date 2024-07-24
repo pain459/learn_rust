963523b695b87869dd4b66a090b53b503becf41d
Pain <ravi.ameer@gmail.com>
2024-07-24 16:47:12 +0530

    Basic mathematics in functions with returns.

--- rust_prog_course/P1/S3/functions_and_code_blocks/src/main.rs
+++ rust_prog_course/P1/S3/functions_and_code_blocks/src/main.rs
@@ -5,8 +5,18 @@
 
 fn main() {
     my_fn("This is from my_fn function!");
+    let str = "This is a custom string passed to the function!";
+    my_fn(str);
+
+    let multiplication_result = basic_multiplication(123, 345);
+    println!("Result is {multiplication_result}");
 }
 
 fn my_fn(s: &str) {
     println!("{s}");
+}
+
+fn basic_multiplication(num1: i32, num2: i32) -> i32 {
+    println!("Computing multiplication!");
+    num1 * num2
 }
