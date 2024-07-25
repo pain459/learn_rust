39ee0a978a1cf68d911d8140f1b1326358c100c4
Pain <ravi.ameer@gmail.com>
2024-07-25 20:05:16 +0530

    if statement in rust.

---
+++ rust_prog_course/P1/S3/conditionals/src/main.rs
@@ -0,0 +1,16 @@
+// -------------------------------------------
+// 			Conditionals
+// 			- If else
+// 			- If else if ladder
+// 			- Match
+// -------------------------------------------
+
+fn main() {
+    println!("We will discuss conditionals in this section.");
+    let num: i8 = 42;
+    if num > 50 {
+        println!("The number is greater than 50");
+    } else {
+        println!("The number is less than 50");
+    }
+}
