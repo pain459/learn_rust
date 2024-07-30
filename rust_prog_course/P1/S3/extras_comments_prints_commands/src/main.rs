aafffa47718eea924888b2f9385662d887a2cf04
Pain <ravi.ameer@gmail.com>
2024-07-30 18:05:27 +0530

    Different types of comments.

---
+++ rust_prog_course/P1/S3/extras_comments_prints_commands/src/main.rs
@@ -0,0 +1,18 @@
+// -------------------------------------------------
+// 			Bonus
+// 			- Comments
+// 			- More on Printing
+//			- Inputs
+//			- Variable Convention
+//			- Statics
+// -------------------------------------------------
+
+fn main() {
+    // First line in the comment
+    // Second line in the comment
+    /*
+    This is a
+    multiline comment
+    */
+    println!("Hello, world!");
+}
