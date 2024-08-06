c868def3785574fe72c9e383675ccd87ec4be50b
pain <ravi.ameer@gmail.com>
2024-08-06 07:38:14 +0530

    test exercise.

--- rust_prog_course/P1/S3/variables_and_constants/src/test.rs
+++ rust_prog_course/P1/S3/variables_and_constants/src/test.rs
@@ -1,6 +1,9 @@
 fn main() {
-    let x = 3;
-    if x == i32::from(true) {
-        println!("Testing if this will print.")
-    }
+    let marks = 98;
+    let grade = match marks {
+        90..=100 => 'A',
+        80..=89 => "B".parse().unwrap(),
+        70..=79 => "C".parse().unwrap(),
+        _ => 'F',
+    };
 }
