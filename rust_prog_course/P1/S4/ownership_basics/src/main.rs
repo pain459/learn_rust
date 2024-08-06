5e5fc8ce2fb6e41b07eda67479f49ee0d39f9066
pain <ravi.ameer@gmail.com>
2024-08-06 08:03:44 +0530

    primitive types demo.

--- rust_prog_course/P1/S4/ownership_basics/src/main.rs
+++ rust_prog_course/P1/S4/ownership_basics/src/main.rs
@@ -10,6 +10,19 @@
 
 fn main() {
     let s1: String = String::from("World!");
-    let s2 = s1;  // use s1.clone() instead to avoid error.
+    let s2 = s1.clone();  // use s1.clone() instead to avoid error.
     println!("s1 is: {s1}");
+
+    // testing ownership in scopes
+    let x: i32 = 123;
+    {
+        let y = x;
+    }
+    // println!("Y is: {y}");  // This statement should give scope error.
+
+    let i: i8 = 12;
+    let j: i8 = i;
+    // This works due to primitive value types.
+    println!("Value of i is {i}");
+    println!("value of j is {j}");
 }
