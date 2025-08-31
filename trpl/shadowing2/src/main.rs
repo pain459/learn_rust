341e2e5c5feacfe496c39d93163fe283039eb2a0
pain <ravi.ameer@gmail.com>
2025-08-31 19:50:09 +0530

    testing shadodwing

---
+++ trpl/shadowing2/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("Total number of spaces are {spaces}");
+
+    // println!("Trying another variation");
+
+    // let spaces2 = "        ";
+    // spaces2 = spaces2.len();
+    // println!("This should throw an error.");
+}
